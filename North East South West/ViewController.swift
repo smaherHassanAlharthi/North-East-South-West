ec11deb18bb2468e831d832204ead7d6f95049b6
smaher <smaher.1416.it@gmail.com>
2021-12-28 21:01:48 +0300

    Initial Commit

---
+++ North East South West/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  North East South West
+//
+//  Created by smaher on 24/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
